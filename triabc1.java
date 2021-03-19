3f3952f6ead6daf4af449d710b7cadb8c0882c6d
fayssal <77855537+fayssalmechmeche@users.noreply.github.com>
2021-03-19 16:21:12 +0100

    Add files via upload

--- triabc1.java
+++ triabc1.java
@@ -1,7 +1,6 @@
-
 public class triabc1 {
-        public static void main(String args[]) {
-                Triangle t1 = new Triangle();
+         public static void main(String args[]) {
+                Triangle t1 = new Triangle(1,2,3);
                 t1.affiche(); 
 
         }
